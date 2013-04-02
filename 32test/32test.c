f01691539a9ef862e99743864e4ce280dd83b7a0
Tony Flores <mexman@gmail.com>
2013-04-01 22:38:09 -0500

    using self settings for init

--- 32test/32test.c
+++ 32test/32test.c
@@ -3,20 +3,21 @@
 #include <util/delay.h>
 
 /* Prototypes */
-void USART0_Init( unsigned int baudrate );
+void USART0_Init(uint32_t bps);
 unsigned char USART0_Receive( void );
 void USART0_Transmit( unsigned char data );
 void USART_putstring(char* StringPtr);
+uint16_t CalculateBaudRate(uint32_t bps);
 
 /* Main - a simple test program*/
 int main( void )
 {
-	USART0_Init( 51 ); /* Set the baudrate to 19,200 bps using a 3.6864MHz crystal */
+	USART0_Init( 250000 ); /* Set the baudrate to 19,200 bps using a 3.6864MHz crystal */
 
 	for(;;) 	    /* Forever */
 	{
 		//USART0_Transmit( USART0_Receive() ); /* Echo the received character */
-		USART_putstring("Hello, world!");
+		USART_putstring("Hello, world!\n");
 		_delay_ms(1000);
 	}
 }
@@ -30,18 +31,39 @@ void USART_putstring(char* StringPtr){
 }
 
 /* Initialize UART */
-void USART0_Init( unsigned int baudrate )
-{
-	/* Set the baud rate */
-	UBRR0H = (unsigned char) (baudrate>>8);
-	UBRR0L = (unsigned char) baudrate;
+//void USART0_Init( unsigned int baudrate )
+//{
+	///* Set the baud rate */
+	//UBRR0H = (unsigned char) (baudrate>>8);
+	//UBRR0L = (unsigned char) baudrate;
+	//
+	///* Enable UART receiver and transmitter */
+	//UCSR0B = ( ( 1 << RXEN0 ) | ( 1 << TXEN0 ) );
+	//
+	///* Set frame format: 8 data 2stop */
+	//UCSR0C = (1<<USBS0)|(1<<UCSZ01)|(1<<UCSZ00);              //For devices with Extended IO
+	////UCSR0C = (1<<URSEL)|(1<<USBS0)|(1<<UCSZ01)|(1<<UCSZ00);   //For devices without Extended IO
+//}
+
+void USART0_Init(uint32_t baud){
 	
-	/* Enable UART receiver and transmitter */
-	UCSR0B = ( ( 1 << RXEN0 ) | ( 1 << TXEN0 ) );
+	//Calculate and set the baud rate
+	uint16_t UBBR1 = CalculateBaudRate(baud);
+	UBRR0H = (unsigned char)(UBBR1>>8);  //4MSBs of Baud Rate in UBBR0H
+	UBRR0L = (unsigned char)UBBR1;		 //8LSBs of Baud Rate in UBBR0L
 	
-	/* Set frame format: 8 data 2stop */
-	UCSR0C = (1<<USBS0)|(1<<UCSZ01)|(1<<UCSZ00);              //For devices with Extended IO
-	//UCSR0C = (1<<URSEL)|(1<<USBS0)|(1<<UCSZ01)|(1<<UCSZ00);   //For devices without Extended IO
+	//Enable USART TX and RX
+	UCSR0B = ((1 << RXEN0) | (1 << TXEN0));
+	
+	//Set Frame Format - 8N1 ASYNC 0 Pol
+	UCSR0C &= ~(1 << UMSEL01) | ~(1 << UMSEL00) | ~(1 << UPM01) | ~(1 << UPM00) | ~(1 << USBS0) | ~(1 << UCSZ02) | (1 << UCSZ01) | (1 << UCSZ00) | ~(1 << UCPOL0);
+	
+}
+
+uint16_t CalculateBaudRate(uint32_t baud){
+	
+	//Calculate UBBR
+	return (F_CPU/(16*baud)) - 1;
 }
 
 
